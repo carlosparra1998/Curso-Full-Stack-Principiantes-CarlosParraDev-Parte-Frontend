46bc6a348e600d2f7583cb8c032eebff6ea4eb4b
carlosparra1998 <cf.pg@hotmail.com>
2026-08-12 12:19:04 +0200

    Models OK

---
+++ lib/app/models/task.dart
@@ -0,0 +1,24 @@
+import 'task_priority.dart';
+
+class Task {
+  late int id;
+  late String title;
+  late bool isComplete;
+  late TaskPriority? priority;
+
+  Task({
+    required this.id,
+    required this.title,
+    required this.isComplete,
+    required this.priority,
+  });
+
+  Task.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    isComplete = json['is_complete'];
+    priority = json['priority'] == null
+        ? null
+        : TaskPriority.fromJson(json['priority']);
+  }
+}
